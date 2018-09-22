ce784f8e516072ae0893c4e1e6e017fbead45f06
Gery Akbar Fauzi <43455599+geryakbarf@users.noreply.github.com>
2018-09-22 10:26:43 +0700

    Add files via upload

---
+++ src/pbo2/pkg10117049/latihan10/logika/PBO210117049Latihan10Logika.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10117049.latihan10.logika;
+
+/**
+ *
+ * @author Gery Akbar Fauzi
+ * Nama                 : Gery Akbar Fauzi
+ * Kelas                : PBO2
+ * Nim                  : 10117049
+ * Deskripsi Program    : Program ini berisi program untuk menampilkan 
+ *                        tipe data logika
+ */
+public class PBO210117049Latihan10Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // deklarasi variabel dengan tipe data logika
+        int umur = 19;
+        boolean dewasa;
+        
+        //Deklarasi jika umur >= 17 tahun maka dewasa = true
+        dewasa = (umur >= 17);
+        System.out.println("Status Dewasa : "+ dewasa);
+    }
+    
+}
